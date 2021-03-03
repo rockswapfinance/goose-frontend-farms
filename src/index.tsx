b4a7d1a00f794b454a46effabef975d5e9de7fd6
luckyplanet42 <luckyplanet@protonmail.com>
2021-03-04 00:45:08 +0100

    feat: Hide site

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import Providers from './Providers'
 
+/*
 ReactDOM.render(
   <React.StrictMode>
     <Providers>
       <App />
     </Providers>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
+*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>
+      soon&trade;
+    </p>
   </React.StrictMode>,
   document.getElementById('root'),
 )
